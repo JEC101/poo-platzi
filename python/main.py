d3f2078f2357baa8ab6c3375700ab0882bc66f57
JEC101 <jecastro.agro@gmail.com>
2021-04-14 08:25:18 -0300

    Método constructor de Python

--- python/main.py
+++ python/main.py
@@ -1,13 +1,9 @@
 from car import Car
+from account import Account
 
 if __name__ == "__main__":
     print("Hola Mundo")
-    car = Car()
-    car.license = "AMS234"
-    car.driver = "Andres Herrera"
+    
+    car = Car("AMS234", Account("Andres Herrera", "AMDx64"))
     print(vars(car))
-
-    car2 = Car()
-    car2.license = "PYG998"
-    car2.driver = "Mathe"
-    print(vars(car2))
+    print(vars(car.driver))
