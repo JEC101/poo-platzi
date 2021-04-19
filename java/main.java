bc8a71c60ea7d7ce33212e179a50f3a951229ef2
JEC101 <jecastro.agro@gmail.com>
2021-04-19 09:55:35 -0300

    Encapsulamiento en Java

--- java/main.java
+++ java/main.java
@@ -1,12 +1,12 @@
 class Main {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
-        Car car = new Car("AMQ123", new Account("Juan Carlos", "AND123"));
-        car.passenger = 4;
-        car.printDataCar();
+        UberX uberX = new UberX("AMQ123", new Account("Juan Carlos", "AND123"), "Chevrolet", "Cruze");
+        uberX.setPassenger(3);
+        uberX.printDataCar();
 
-        Car car2 = new Car("QWE567", new Account("Andres Herrera", "ANSA876"));
-        car2.passenger = 3;
-        car2.printDataCar();
+        // Car car2 = new Car("QWE567", new Account("Andres Herrera", "ANSA876"));
+        // car2.passenger = 3;
+        // car2.printDataCar();
     }
 }
